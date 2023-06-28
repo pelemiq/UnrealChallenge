fc54c679c55b0c1c5375e4304246c2582f7df665
Pepe Lemos <pelemiq@gmail.com>
2023-06-29 00:03:32 +0200

    unreal project created

---
+++ Source/UnrealChallenge/UnrealChallengeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealChallengeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCHALLENGE_API AUnrealChallengeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
